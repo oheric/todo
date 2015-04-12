2fef122a43330955382a1b950189dee210acd9ac
oheric <ohericyoung@gmail.com>
2015-04-11 17:23:40 -0700

    Add user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,28 @@
 require "rails_helper"
+# we've included shouldamatchers in our gemfile, but wrote require:false, so we must explcitly require it here. 
+require "shoulda/matchers"
+
 describe User do
+
+  # it "has an email" do 
+  #   user = FactoryGirl.create(:a_user)
+  #   expect(user.email).to_not be_nil
+  # end
+
+  # it "has a password" do 
+  #   user = FactoryGirl.create(:a_user)
+  #   expect(user.password).to_not be_nil 
+  # end
+
+  it { should validate_uniqueness_of(:email) }
+
+  it do 
+    should allow_value('a@a.com').
+      for(:email)
+  end 
+
+  it { should ensure_length_of(:password).is_at_least(6) }
+  it { should ensure_length_of(:password).is_at_most(20) }
+  it { should ensure_length_of(:email).is_at_most(255) }
+  
 end
